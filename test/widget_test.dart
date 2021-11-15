71582871b8d0cc333bde6df072d73ee76cca1974
Adeniran <gaphcen4better@gmail.com>
2021-11-15 17:27:32 +0100

    second commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:my_shopper_design/main.dart';
+import 'package:shopper_design/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
